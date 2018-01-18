1fa758d1345b16d0ce69942371a9136578a3bf07
Ian Darwin <ian@darwinsys.com>
2018-01-18 10:23:37 -0500

    Add close function. Use abs path for external projects!

--- src/createallprojects/parts/CreateProjects.java
+++ src/createallprojects/parts/CreateProjects.java
@@ -1,92 +1,67 @@
 package createallprojects.parts;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
 
 public class CreateProjects {
     
-    /** List of projects to create.
-     * This will need to be read from a file, which 
-     * makehandsons will generate the starting version of
-     */
-    List<String> projectNames = Arrays.asList(
-    	    "donow31",
-    	    "donow31solution",
-    	    "donow21",
-    	    "donow21solution",
-    	    "ember",
-    	    "embersolution",
-    	    "ex21",
-    	    "ex21solution",
-    	    "ex31",
-    	    "ex31solution",
-    	    "ex41",
-    	    "ex41solution",
-    	    "ex42",
-    	    "ex42solution",
-    	    "ex51",
-    	    "ex51solution",
-    	    "ex52",
-    	    "ex52solution",
-    	    "ex53",
-    	    "ex53client",
-    	    "ex53clientsolution",
-    	    "ex53solution",
-    	    "ex61",
-    	    "ex61solution",
-    	    "ex71",
-    	    "ex71solution",
-    	    "ex81",
-    	    "ex81solution",
-    	    "minimal",
-    	    "samples",
-    	    "samples_ee8"
-    	);
-    
-
-    ExecutorService threadPool = Executors.newFixedThreadPool(5);
-    
+    IWorkspace workspace = ResourcesPlugin.getWorkspace();
+	IWorkspaceRoot root = workspace.getRoot();
+	
 	public void doCreates() {
-	    	System.out.println("CreateProjects.execute()");
-	    	IWorkspace workspace = ResourcesPlugin.getWorkspace();
-	    	IWorkspaceRoot root = workspace.getRoot();
-	    	for (String name : projectNames) {
-	    		IProject project = root.getProject(name);
-	    		try {
-	    			if (!project.exists()) {
-	    				project.create(new NullProgressMonitor());
-	    			}
+			System.out.println("CreateProjects.execute()");
+
+			for (ProjDesc pd : ProjectList.getProjects()) {
+			String name = pd.name;
+			String path = pd.path;
+			IPath projectLocation = null;
+			if (path != null) {
+				projectLocation = new Path(path);
+			}
+				IProject project = root.getProject(name);
+				try {
+					if (!project.exists()) {
+						IProjectDescription description =
+							ResourcesPlugin.getWorkspace().newProjectDescription(project.getName());
+						description.setLocation(projectLocation);
+						project.create(description, null);
+					}
 					project.open(null);	// verifies the project
 					IPath rawLocation = project.getRawLocation();
 					if (rawLocation == null) {
 						System.err.println("Warning: Project %s exists but has no location");
 					}
-					project.refreshLocal(-1, null); // Force refreshing even if proj existed.
+					project.refreshLocal(IResource.DEPTH_INFINITE, null); // Force refreshing even if proj existed.
 
-	    			// Close project after a few seconds, give it time to update, sync, etc.
-	    			threadPool.submit( () -> { 
-	    				try {
-						Thread.sleep(5 * 1000);
-						project.close(null); 
-					} catch (InterruptedException | CoreException e) {
-						throw new RuntimeException("Close thread failed: " + e, e);
-					}
-	    			} );
-	    		} catch (CoreException e) {
-	    			System.out.println(e);
-	    		}
-	
-	    	}
-	    	System.out.println("CreateProjects.execute() done");
+				} catch (CoreException e) {
+					System.err.println("Error creating/refreshing " + name + "; " + e);
+				}
+			}
+			System.out.println("CreateProjects.execute() done");
+	}
+
+	public void doCloseAll() {
+		for (ProjDesc pd : ProjectList.getProjects()) {
+			String projectName = pd.name;
+			IProject project = root.getProject(projectName);
+
+			if (!project.exists()) {
+				System.err.println("Project not open: " + projectName);
+			}
+			if (project.isOpen()) {
+				try {
+					project.close(null);
+				} catch (CoreException e) {
+					System.err.println("Error in closing " + projectName + "; " + e);
+				}
+			}
+		}
 	}
 }
